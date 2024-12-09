b272c87cb16a649d155b9e5c9147b3d1eb3a76d1
Katanon Krengsomrong <66160196@go.buu.ac.th>
2024-12-10 01:03:59 +0700

    HTML CRUD User

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,13 @@ import { RouterLink, RouterView } from 'vue-router'
 <template>
   <header>
     <nav>
-      <RouterLink to="/">Home</RouterLink> |
-      <RouterLink to="/todos">Todos</RouterLink> |
-      <RouterLink to="/about">About</RouterLink> 
+      <RouterLink to="/">Home</RouterLink> | <RouterLink to="/todos">Todos</RouterLink> |
+      <RouterLink to="/users">Users</RouterLink> |
+      <RouterLink to="/about">About</RouterLink>
     </nav>
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-
-</style>
+<style scoped></style>
